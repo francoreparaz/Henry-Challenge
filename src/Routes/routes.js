8a8dfecc6c4aaa193ac419a2ecbbe5778a2e784f
Franco <you@example.com>
2020-11-03 14:46:53 -0300

    ruta api search

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,5 +1,22 @@
-const {Router}= require ("express");
+const {Router, query}= require ("express");
 const router= Router();
- 
-module.exports = router
+const axios=require ("axios");
+
+router.get("/api/search",(req,res)=>{
+    const element = req.query.q;
+    axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${element}`)
+    .then((response) => {
+        const result={response:response.data.results}
+        res.send(result).status(200)
+        
+    })
+    
+    .catch((error) => {
+        console.log(error);
+        res.send(error)
+    });
+    
+})
+
+module.exports = router;
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express= require("express");
 const app= express();
-
+const bodyParser = require('body-parser');
 //Middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+//app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+//app.use(bodyParser.json({ limit: '50mb' }));
 
 //routes
 app.use(require("./Routes/routes"));
