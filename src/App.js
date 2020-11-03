174f2aa0a635fd6f0ac2bcb4ef95449a9290c899
Franco <you@example.com>
2020-11-03 16:18:56 -0300

    componente searchbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+//cimporting components
+import SearchBar from "./components/searchBar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <SearchBar/>
      <h1>Henry Challenge</h1>
     </div>
   );
