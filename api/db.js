160632099246172d825e3ca62d7133d38924ef7f
Franco <you@example.com>
2020-11-03 21:41:27 -0300

    peticion a ruta backend

--- api/db.js
+++
@@ -1,6 +0,0 @@
-const {Sequelize}= require("sequelize");
-const sequelize = new Sequelize("postgres://postgres:mishermosos@localhost:5432/challenge")
-
-sequelize.authenticate()
-.then(()=>{console.log("conectado")})
-.catch(err=>{console.log("ERROR",err)})
